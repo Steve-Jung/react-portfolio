1fd896d03c33b03c3a4c74c65a45e25c19749a05
Steve-Jung <jung25820@gmail.com>
2021-07-11 22:58:45 +0900

    change display

--- components/mainPage/MainContents.js
+++ components/mainPage/MainContents.js
@@ -15,7 +15,7 @@ const MainContents = memo(({ start, currentCamera }) => {
 		if (display === true) {
 			gsap.to(ref.current, {
 				duration: 1.8,
-				visibility: 'visible',
+				display: 'flex',
 				opacity: 1,
 				xPercent: 0,
 				ease: 'power3.out',
@@ -24,7 +24,7 @@ const MainContents = memo(({ start, currentCamera }) => {
 			gsap.timeline()
 				.to(ref.current, {
 					duration: 0.9,
-					visibility: 'hidden',
+					display: 'none',
 					opacity: 0,
 					xPercent: 50,
 					ease: 'power3.out',
