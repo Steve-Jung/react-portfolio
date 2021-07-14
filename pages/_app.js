730f830085cb09f112a8657e16a59d92b914a7eb
Steve-Jung <jung25820@gmail.com>
2021-07-15 08:54:52 +0900

    change Header info

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import PropTypes from 'prop-types';
 import { AnimatePresence } from 'framer-motion';
 
+import Header from '../components/Header';
+
 // Styles
 import 'simplebar/dist/simplebar.min.css';
 import '../styles/App.scss';
@@ -21,14 +23,7 @@ const App = ({ Component, pageProps, router }) => {
 
   return (
     <>
-      <Head>
-        <title>JungJisub</title>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
-        <meta name="author" content="JungJisub" />
-        <meta name="description" content="사용자를 매료시킬 수 있는 흥미로운 디자인과 오래도록 머물고 싶어지는 편안한 UI를 만들기 위해 늘 고심하는 개발자 정지섭의 포트폴리오 사이트입니다." />
-      </Head>
+      <Header />
       <AnimatePresence exitBeforeEnter>
         <Component isFirstMount={isFirstMount} key={router.route} {...pageProps} />
       </AnimatePresence>
