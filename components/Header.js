3cd238a5215a96d1a0457fc60903f7077d8f956d
Steve-Jung <jung25820@gmail.com>
2021-07-15 09:03:49 +0900

    change Header info

--- components/Header.js
+++ components/Header.js
@@ -13,14 +13,14 @@ const Header = ({ subtitle, page }) => {
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="정지섭의 포트폴리오" />
-      <meta property="og:description" content="정지섭의 포트폴리오 사이트입니다" />
-      {/* <meta property="og:image" content={`${MainPageImage.src}`} /> */}
-      <meta property="og:url" content={page ? `https://jungjisub.vercel.app${page}` : 'https://jungjisub.vercel.app'} />
       <meta name="keywords" content="정지섭 포트폴리오" />
       <meta name="author" content="JungJisub" />
       <meta name="description" content="사용자를 매료시킬 수 있는 흥미로운 디자인과 오래도록 머물고 싶어지는 편안한 UI를 만들기 위해 늘 고심하는 개발자 정지섭의 포트폴리오 사이트입니다." />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="정지섭의 포트폴리오" />
+      <meta property="og:description" content="정지섭의 포트폴리오 사이트입니다" />
+      <meta property="og:image" content={`https://jungjisub.vercel.app/${MainPageImage.src}`} />
+      <meta property="og:url" content={page ? `https://jungjisub.vercel.app${page}` : 'https://jungjisub.vercel.app'} />
     </Head>
   )
 }
